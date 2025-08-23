7f3f25c0b0df6bdeff1d4f2941158dfc28271c3a
dachi <d_zedginidze@cu.edu.ge>
2025-08-23 21:02:35 +0400

    jwt auth added

--- courses/urls.py
+++ courses/urls.py
@@ -19,7 +19,7 @@ router.register(r'grade-comments', GradeCommentViewSet, basename='grade-comment'
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('', include(router.urls)),
